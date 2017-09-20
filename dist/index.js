6627c793a81fdf3c2124f182f6315070ba740b15
Demven <dzmitry_salnikau@epam.com>
2017-09-20 14:55:24 -0400

    Build files for version 2.1.0;

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.MOUSE_RIGHT_BUTTON = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -27,6 +28,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var MOUSE_RIGHT_BUTTON = exports.MOUSE_RIGHT_BUTTON = 2;
 
 var TrackingLink = function (_Component) {
   _inherits(TrackingLink, _Component);
@@ -113,6 +116,11 @@ var TrackingLink = function (_Component) {
       var event = ev.nativeEvent;
 
       if (this.state.preventTouchTap) {
+        return false;
+      }
+
+      // on Windows right click triggers touchTap event (react-tap-event-plugin bug)
+      if (event.button === MOUSE_RIGHT_BUTTON) {
         return false;
       }
 
