3f780fd1ecce15ac3c166ea5a99a877356f646b4
Demven <dzmitry_salnikau@epam.com>
2017-09-22 17:58:02 -0400

    Delete dist folder from repository;

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 export const MOUSE_RIGHT_BUTTON = 2;
